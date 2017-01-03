7b9f4af3097bc255494a604687977d95e82202a5
akai-tsuki <akai.tsuki.mobile@gmail.com>
2017-01-04 02:07:52 +0900

    init commit

---
+++ src/main/java/sample/akai/tsuki/controller/HelloController.java
@@ -0,0 +1,16 @@
+package sample.akai.tsuki.controller;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/hello")
+	public Map<String, String> hello() {
+		return Collections.singletonMap("message", "Hello world");
+	}
+}
